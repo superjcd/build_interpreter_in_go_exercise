a68de780f38ce8c81c92bc16c0634e7fd857a3d9
superjcd <929760274@qq.com>
2022-12-12 09:31:52 +0800

    3.4

--- object/object.go
+++ object/object.go
@@ -8,6 +8,8 @@ type ObjectType string
 
 const (
 	INTEGER_OBJ = "INTEGER"
+	BOOLEAN_OBJ = "BOOLEAN"
+	NULL_OBJ = "NULL"
 )
 
 type Object interface {
@@ -19,6 +21,12 @@ type Integer struct {
 	Value int64
 }
 
-func (i *Integer) Inspect() string { return fmt.Sprintf("%d", i.Value) }
+type Boolean struct {
+	Value bool
+}
 
+func (i *Integer) Inspect() string  { return fmt.Sprintf("%d", i.Value) }
 func (i *Integer) Type() ObjectType { return INTEGER_OBJ }
+
+func (b *Boolean) Type() ObjectType { return BOOLEAN_OBJ }
+func (b *Boolean) Inspect() string  { return fmt.Sprintf("%t", b.Value) }
